9a5a93f5b6f6bfa22ffb7237dc9e412e0f9f8b3b
shingorow <shingo.m1217@gmail.com>
2022-03-14 19:30:19 +0900

    feature/deployer追加

---
+++ deploy.php
@@ -0,0 +1,36 @@
+<?php
+namespace Deployer;
+
+require 'recipe/common.php';
+
+// Project name
+set('application', 'saimitsu.net');
+
+// Project repository
+set('repository', 'git@github.com:blogle2022/saimitsunet.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true);
+
+// Shared files/dirs between deploys
+set('shared_files', []);
+set('shared_dirs', []);
+
+// Writable dirs by web server
+set('writable_dirs', []);
+
+
+// Hosts
+
+host('59.106.19.189')
+    ->user('saimitsucom');
+
+
+// Tasks
+
+desc('Deploy your project');
+task('deploy:dev', function () {
+    cd('~/www/saimitsudev');
+    $pwd = run('pwd');
+    writeln($pwd);
+});
